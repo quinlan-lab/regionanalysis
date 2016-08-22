83f31a19d58e94dea02fe2211568c36367d002e7
Jim Havrilla <jmh2tt@cphgs-C02N30EZFD59.local>
2016-08-21 18:25:41 -0600

    added parsing for deligt sets

--- generatedenovobeds.sh
+++ generatedenovobeds.sh
@@ -2,4 +2,6 @@ tr -s "" "\n" < ogfiles/homsy_database_S02.txt | grep -v '\t\t\t' | cut -f 2- |
 
 tr -s "" "\n" < ogfiles/homsy_database_S03.txt | grep -v '\t\t\t' | cut -f 2- | sed '1d' | awk '{t=$2-1} {printf $1"\t"t"\t"$2} {for (i=3; i<=NF; i++) printf "\t"$i} {print ""}' | perl -pe 's/Start\s*lost/Start lost/g' | perl -pe 's/Splice\s*site/Splice site/g' | perl -pe 's/Stop\s*lost/Stop lost/g' | grep -v 'Splice site' |  perl -pe 's/\*.*//g' | grep 'D-Missense' > ~/analysis/denovos/homsycontrol.bed
 
+tr -s "" "\n" < ogfiles/deligtetal.txt | grep -v "p.(=)" | sed '1d' | grep -v '\t\t\t\t\t\t\t\t' | cut -f 4- | sed 's/^chr//g' | grep -w -v 'NO' > ~/analysis/denovos/deligt.bed
 
+tr -s "^M" "\n" < ogfiles/deligtetal.txt | grep -v "p.(=)" | sed '1d' | grep -v '\t\t\t\t\t\t\t\t' | cut -f 4- | sed 's/^chr//g' | grep -w 'NO' > ~/analysis/denovos/deligtcontrol.bed
