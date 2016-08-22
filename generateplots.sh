e5d8d34366fb228a359e717e981c25d05c3b24d4
James Havrilla <u1021864@kingspeak23.wasatch.peaks>
2016-08-21 21:19:37 -0600

    easier to compare top and mid regions, will find more appropriate way

--- generateplots.sh
+++ generateplots.sh
@@ -1,9 +1,15 @@
 set -x
 DATA=/scratch/ucgd/lustre/u1021864/serial
-REGIONS=$1
-HOTSPOT=$2
-FOLDER=$3
-OUT=$FOLDER/$FOLDER'_'$(echo $HOTSPOT | rev | cut -f 1 -d "/" | rev | cut -f 1 -d ".")'.overlap.txt'
+REGIONS=regions/topresid.txt
+HOTSPOT=$1
+FOLDER=top
+function runplot {
+  OUT=$FOLDER/$FOLDER'_'$(echo $HOTSPOT | rev | cut -f 1 -d "/" | rev | cut -f 1 -d ".")'.overlap.txt'
 
-$HOME/software/poverlap/poverlap.py poverlap --a $REGIONS --b $HOTSPOT -g $DATA/human.hg19.genome --include $DATA/Homo_sapiens.GRCh37.75.gtf.gz -N 100 > results/$OUT
-python plot.py results/$OUT $FOLDER
+  $HOME/software/poverlap/poverlap.py poverlap --a $REGIONS --b $HOTSPOT -g $DATA/human.hg19.genome --include $DATA/Homo_sapiens.GRCh37.75.gtf.gz -N 100 > results/$OUT
+  python plot.py results/$OUT $FOLDER
+}
+runplot
+REGIONS=regions/midresid.txt
+FOLDER=mid
+runplot
