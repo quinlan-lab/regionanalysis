118fc6ec739d292aa3fc9dabf7c93103e2fc6751
James Havrilla <u1021864@kingspeak2.wasatch.peaks>
2016-08-25 15:25:21 -0600

    added comments updated script

--- generatedenovobeds.sh
+++ generatedenovobeds.sh
@@ -1,11 +1,14 @@
+# converting Homsy files, filtering out non "D-missense variants"
 tr -s "" "\n" < ogfiles/homsy_database_S02.txt | grep -v '\t\t\t' | cut -f 2- | sed '1d' | awk '{t=$2-1} {printf $1"\t"t"\t"$2} {for (i=3; i<=NF; i++) printf "\t"$i} {print ""}' | perl -pe 's/Start\s*lost/Start lost/g' | perl -pe 's/Splice\s*site/Splice site/g' | perl -pe 's/Stop\s*lost/Stop lost/g' | grep -v 'Splice site' | perl -pe 's/\*.*//g' | grep 'D-Missense' > ~/analysis/denovos/homsy.bed
 
 tr -s "" "\n" < ogfiles/homsy_database_S03.txt | grep -v '\t\t\t' | cut -f 2- | sed '1d' | awk '{t=$2-1} {printf $1"\t"t"\t"$2} {for (i=3; i<=NF; i++) printf "\t"$i} {print ""}' | perl -pe 's/Start\s*lost/Start lost/g' | perl -pe 's/Splice\s*site/Splice site/g' | perl -pe 's/Stop\s*lost/Stop lost/g' | grep -v 'Splice site' |  perl -pe 's/\*.*//g' | grep 'D-Missense' > ~/analysis/denovos/homsycontrol.bed
 
+# converting deLigt files, filtering out synonymous variants
 tr -s "" "\n" < ogfiles/deligtetal.txt | grep -v "p.(=)" | sed '1d' | grep -v '\t\t\t\t\t\t\t\t' | cut -f 4- | sed 's/^chr//g' | grep -w -v 'NO' > ~/analysis/denovos/deligt.bed
 
 tr -s "" "\n" < ogfiles/deligtetal.txt | grep -v "p.(=)" | sed '1d' | grep -v '\t\t\t\t\t\t\t\t' | cut -f 4- | sed 's/^chr//g' | grep -w 'NO' > ~/analysis/denovos/deligtcontrol.bed
 
-tr -s "" "\n" < ogfiles/mcraeetal.txt | sed '1d' | cut -f 3- | awk '{t=$2-1} {printf $1"\t"t"\t"$2} {for (i=3; i<=NF; i++) printf "\t"$i} {print ""}' | awk '$8<.5' > denovos/mcraecontrol.bed
+# converting McRae files, filtering out synonymous variants
+tr -s "" "\n" < ogfiles/mcraeetal.txt | sed '1d' | cut -f 3- | awk '{t=$2-1} {printf $1"\t"t"\t"$2} {for (i=3; i<=NF; i++) printf "\t"$i} {print ""}' | awk '$8<.5' | grep -v -Pw "synonymous_variant|splice_donor_variant|splice_acceptor_variant|splice_region_variant" > denovos/mcraecontrol.bed
 
-tr -s "" "\n" < ogfiles/mcraeetal.txt | sed '1d' | cut -f 3- | awk '{t=$2-1} {printf $1"\t"t"\t"$2} {for (i=3; i<=NF; i++) printf "\t"$i} {print ""}' | awk '$8>.5' > denovos/mcrae.bed
+tr -s "" "\n" < ogfiles/mcraeetal.txt | sed '1d' | cut -f 3- | awk '{t=$2-1} {printf $1"\t"t"\t"$2} {for (i=3; i<=NF; i++) printf "\t"$i} {print ""}' | awk '$8>.5' | grep -v -Pw "synonymous_variant|splice_donor_variant|splice_acceptor_variant|splice_region_variant" > denovos/mcrae.bed
--- generateplots.sh
+++ generateplots.sh
@@ -1,7 +1,9 @@
 set -x
 DATA=/scratch/ucgd/lustre/u1021864/serial
 REGIONS=regions/topresid.txt
+# pass your file for montecarlo analysis, could be pathogenic variants, benign variants, whatever
 HOTSPOT=$1
+# first runs analysis on top residuals
 FOLDER=top
 function runplot {
   OUT=$FOLDER/$FOLDER'_'$(echo $HOTSPOT | rev | cut -f 1 -d "/" | rev | cut -f 1 -d ".")'.overlap.txt'
@@ -9,6 +11,7 @@ function runplot {
   $HOME/software/poverlap/poverlap.py poverlap --a $REGIONS --b $HOTSPOT -g $DATA/human.hg19.genome --include $DATA/Homo_sapiens.GRCh37.75.gtf.gz -N 100 > results/$OUT
   python plot.py results/$OUT $FOLDER
 }
+# then runs analysis on expected regions, regions on the model line "middle residuals"
 runplot
 REGIONS=regions/midresid.txt
 FOLDER=mid
