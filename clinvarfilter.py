88c34b5dc46b1170c66dd5d05e015e66474a9a90
James Havrilla <u1021864@kingspeak23.wasatch.peaks>
2016-10-17 13:32:44 -0600

    break to make sure the same clinvar position is not added more than once, unless it is multiallelic

--- clinvarfilter.py
+++ clinvarfilter.py
@@ -3,8 +3,9 @@ import sys
 import exacresiduals.utils as u
 reload(sys)
 sys.setdefaultencoding("ISO-8859-1")
+#this script makes appropriate pathogenic and benign clinvar variant files
 
-clinvar=VCF(sys.argv[1]) #clinvar file, vcfanno'd and vep'd
+clinvar=VCF(sys.argv[1]) #clinvar file, vcfanno'd and vep'd: /scratch/ucgd/lustre/u1021864/serial/clinvar-anno.vep.vcf.gz
 kcsq = clinvar["CSQ"]["Description"].split(":")[1].strip(' "').split("|")
 f1=open('benign.vcf','wb')
 f2=open('patho.vcf','wb')
@@ -21,5 +22,11 @@ for variant in clinvar:
         if not u.isfunctional(csq): continue
         if info['CLNSIG'] == '2':
             f1.write(str(variant))
+            break
         if info['CLNSIG'] == '5':
+            try:
+                if info['max_aaf_all']>0.01: continue
+            except:
+                pass
             f2.write(str(variant))
+            break
