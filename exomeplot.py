2efd3fb6c6b897c433570e25067977834ec86d14
James Havrilla <u1021864@kingspeak23.wasatch.peaks>
2017-01-24 17:47:47 -0700

    fixes and finishing plotting script

--- exomeplot.py
+++ exomeplot.py
@@ -7,29 +7,31 @@ import numpy as np
 import toolshed as ts
 
 lens=[]
-it = ts.reader(sys.argv[1]) #$DATA/unfilteredregions.txt
+it = ts.reader(sys.argv[1]) #$HOME/analysis/exacresiduals/results/2016_12_10/unfilteredresiduals.txt
 iterable = (i for i in it)
 for region in iterable:
     lens.append(int(region['end'])-int(region['start']))
 
-print sum(lens)
+plt.hist(lens,500,facecolor='green')
+plt.savefig('funchist.png',bbox_inches='tight')
+
 totlen=0.0; ordered=[]; exomecov=[]; wgtdpct=[]
 it = ts.reader(sys.argv[2]) #$HOME/analysis/exacresiduals/results/2016_12_10/weightedresiduals.txt (sorted by weighted_pct)
 iterable = (i for i in it)
 for region in iterable:
     length=int(region['end'])-int(region['start'])
     totlen+=length
-    ordered.append((length,region['weighted_pct']))
+    ordered.append((length,float(region['weighted_pct']),region['ranges']+region['gene']))
 ordered=sorted(ordered,key=lambda x:x[1])
 length=0.0
-it = ts.reader(sys.argv[2])
-iterable = (i for i in it)
 prevregion=None
 for region in ordered:
-    length+=(int(region['end'])-int(region['start']))
-    if region['ranges']+region['gene']!=prevregion or prevregion!=None:
+    length+=region[0]
+    if ordered[2]!=prevregion and prevregion!=None:
         exomecov.append(prevvalues[0])
         wgtdpct.append(prevvalues[1])
-    prevregion=region['ranges']+region['gene']
-    prevvalues=(length/totlen,region['weighted_pct'])
-print exomecov, wgtdpct
+    prevregion=region[2]
+    prevvalues=(length/totlen,region[1])
+plt.figure()
+plt.plot(wgtdpct,exomecov)
+plt.savefig('exomecov.png',bbox_inches='tight')
