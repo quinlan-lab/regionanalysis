0ddd5c535a1fd9512ed3bf90a4d45589d2537606
James Havrilla <u1021864@kingspeak23.wasatch.peaks>
2017-02-08 00:00:12 -0700

    conservation data for top 1%, for discovery of new regions of interest

---
+++ conscomparison.sh
@@ -0,0 +1,5 @@
+python addscores.py
+python comparison.py > means.txt
+awk 'FNR==NR{gerp=$1; phast=$2; cadd=$3} FNR!=NR{n=split($(NF-2),a,","); for (i in a) sum+=a[i]; if (sum/n < gerp && $12 > 99) print; sum=0}' means.txt $HOME/analysis/scoredregions.bed > lowgerp.txt
+awk 'FNR==NR{gerp=$1; phast=$2; cadd=$3} FNR!=NR{n=split($(NF-1),a,","); for (i in a) sum+=a[i]; if (sum/n < phast && $12 > 99) print; sum=0}' means.txt $HOME/analysis/scoredregions.bed > lowphast.txt
+awk 'FNR==NR{gerp=$1; phast=$2; cadd=$3} FNR!=NR{n=split($NF,a,","); for (i in a) sum+=a[i]; if (sum/n < cadd && $12 > 99) print; sum=0}' means.txt $HOME/analysis/scoredregions.bed > lowcadd.txt
