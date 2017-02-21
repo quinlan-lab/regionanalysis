c35426a9ac0570d76e8c08640af0c5ff8a4aa0e9
James Havrilla <u1021864@kingspeak23.wasatch.peaks>
2017-02-21 16:01:15 -0700

    committing before making a bigger change

--- conscomparison.py
+++ conscomparison.py
@@ -13,18 +13,18 @@ import cPickle as pickle
 import os.path
 
 def read_values(path='/uufs/chpc.utah.edu/common/home/u1021864/analysis/scoredregions.bed'):
-    gerp,phast,cadd,topgerp,topphast,topcadd,toptengerp,toptenphast,toptencadd,ccrpct,topccr=[],[],[],[],[],[],[],[],[],[],[]
+    gerp,phast,cadd,topgerp,topphast,topcadd,toptengerp,toptenphast,toptencadd,ccrs=[],[],[],[],[],[],[],[],[],[]
     for i, region in enumerate(ts.reader(path, header="ordered")):
         g = np.mean(map(float,region['GERP'].split(",")))
         p = np.mean(map(float,region['phastCons'].split(",")))
         c = np.mean(map(float,region['CADD'].split(",")))
-        pct=float(region['weighted_pct']); gene=region['gene']
-        gerp.append(g);phast.append(p);cadd.append(c);ccrpct.append((gene,pct))
+        pct=float(region['weighted_pct']); gene=region['gene']; chrom=region['chrom']; ranges=region['ranges']
+        gerp.append(g);phast.append(p);cadd.append(c);ccrs.append((chrom,ranges,gene,pct))
         if pct>=90:
-            toptengerp.append(g);toptenphast.append(p);toptencadd.append(c);topccr.append((gene,pct))
+            toptengerp.append(g);toptenphast.append(p);toptencadd.append(c)
             if pct>=99:
                 topgerp.append(g);topphast.append(p);topcadd.append(c)
-    return gerp,phast,cadd,topgerp,topphast,topcadd,toptengerp,toptenphast,toptencadd,ccrpct,topccr
+    return gerp,phast,cadd,topgerp,topphast,topcadd,toptengerp,toptenphast,toptencadd,ccrs
 
 def barplot(dataset):
     mi,ma=min(dataset),max(dataset)
@@ -46,14 +46,14 @@ def clinvar(path='patho.vcf'):
 
 
 if not os.path.isfile("var.pickle"):
-    gerp,phast,cadd,topgerp,topphast,topcadd,toptengerp,toptenphast,toptencadd,ccrpct,topccr=read_values('scoredregions.bed')
+    gerp,phast,cadd,topgerp,topphast,topcadd,toptengerp,toptenphast,toptencadd,ccrs=read_values('scoredregions.bed')
     #genes=clinvar('patho.vcf') #patho.vcf, vcf file after clinvarfilter.py for pathogenic variants
     var={}
-    var['gerp']=gerp; var['phast']=phast; var['cadd']=cadd; var['toptengerp']=toptengerp; var['toptenphast']=toptenphast; var['toptencadd']=toptencadd; var['topgerp']=topgerp; var['topphast']=topphast; var['topcadd']=topcadd; var['ccrpct']=ccrpct; var['topccr']=topccr
+    var['gerp']=gerp; var['phast']=phast; var['cadd']=cadd; var['toptengerp']=toptengerp; var['toptenphast']=toptenphast; var['toptencadd']=toptencadd; var['topgerp']=topgerp; var['topphast']=topphast; var['topcadd']=topcadd; var['ccrs']=ccrs
     pickle.dump(var, open("var.pickle", "wb"))
 
 var = pickle.load(open("var.pickle", "rb"))
-gerp=var['gerp']; phast=var['phast']; cadd=var['cadd']; toptengerp=var['toptengerp']; toptenphast=var['toptenphast']; toptencadd=var['toptencadd']; topgerp=var['topgerp']; tophphast=var['topphast']; topcadd=var['topcadd']; ccrpct=var['ccrpct']; topccr=var['topccr']
+gerp=var['gerp']; phast=var['phast']; cadd=var['cadd']; toptengerp=var['toptengerp']; toptenphast=var['toptenphast']; toptencadd=var['toptencadd']; topgerp=var['topgerp']; tophphast=var['topphast']; topcadd=var['topcadd']; ccrs=var['ccrs']
 
 fig, axarr = plt.subplots(2,3)
 fig.tight_layout()
@@ -114,8 +114,21 @@ fig.tight_layout()
 #color = ["white" if x[0] in genes else "red" for x in topccr]
 #color = ["red" if x < np.nanmean(ccrpct) else "blue" if x > 90 else "black" for x in ccrpct]
 #ind = [i for i,x in enumerate(ccrpct) if x[0] not in genes]
-ccrpct = [x[1] for x in ccrpct]
+ccrpct = [x[3] for x in ccrs]
 #ccrpct = [ccrpct[i] for i in ind]; gerp=[gerp[i] for i in ind]; phast=[phast[i] for i in ind]; cadd=[cadd[i] for i in ind]
+
+key=None;gerpsum=0;phastsum=0;caddsum=0
+for i,j,k,l in zip(gerp,phast,cadd,ccrs):
+    if key == None or key != l[0]+l[1]+l[2]:
+        if i > np.nanmean(gerp) and l[3] < np.nanmean(ccrpct):
+            gerpsum+=1
+        if j > np.nanmean(phast) and l[3] < np.nanmean(ccrpct):
+            phastsum+=1
+        if k > np.nanmean(cadd) and l[3] < np.nanmean(ccrpct):
+            caddsum+=1
+    key=l[0]+l[1]+l[2]
+print gerpsum,phastsum,caddsum
+        
 
 axarr[0].set_xlim(0,100)
 axarr[1].set_xlim(0,100)
