6c10cf20ba21521ca634dacbdc43f688c565eadd
James Havrilla <u1021864@kingspeak23.wasatch.peaks>
2017-02-22 02:25:50 -0700

    edits and comments to genebar, random->expected

--- genebar.py
+++ genebar.py
@@ -13,9 +13,10 @@ def transfrac(fraction):
 
 for line in f:
     fields=line.strip().split("\t")
-    frac1=transfrac(fields[1]); frac2=transfrac(fields[2]); frac3=transfrac(fields[3])
-    frac4=transfrac(fields[4]); frac5=transfrac(fields[5]); frac6=transfrac(fields[6])
-    d[fields[0]]=frac1,frac2,frac3,frac4,frac5,frac6
+    frac1=transfrac(fields[1]); frac2=transfrac(fields[2]);
+    #frac3=transfrac(fields[3]); frac4=transfrac(fields[4]);
+    frac5=transfrac(fields[5]); frac6=transfrac(fields[6])
+    d[fields[0]]=frac1,frac2,frac5,frac6#frac3,frac4,frac5,frac6
 
 N = len(d)
 ind = np.arange(N)
@@ -28,15 +29,17 @@ rects1 = ax.barh(ind,[i[0] for i in d.values()], width-.05, color = 'w', lw=2, a
 rects2 = ax.barh(ind,[i[1] for i in d.values()], width-.05, color = 'k', lw=2, alpha=0.8)
 rects3 = ax.barh(ind+width,[i[2] for i in d.values()], width-.05, color = 'dodgerblue', lw=2, alpha=0.7) # hatch='|')
 rects4 = ax.barh(ind+width,[i[3] for i in d.values()], width-.05, color = 'k', lw=2, alpha=0.8)
-rects5 = ax.barh(ind+width*2,[i[4] for i in d.values()], width-.05, color = 'darkorange', lw=2, alpha=0.7) # hatch='//')
-rects6 = ax.barh(ind+width*2,[i[5] for i in d.values()], width-.05, color = 'k', lw=2, alpha=0.8)
+#rects5 = ax.barh(ind+width*2,[i[4] for i in d.values()], width-.05, color = 'darkorange', lw=2, alpha=0.7) # hatch='//')
+#rects6 = ax.barh(ind+width*2,[i[5] for i in d.values()], width-.05, color = 'k', lw=2, alpha=0.8)
 ax.set_yticks(ind+width+.05)
 ax.set_yticklabels([i for i in d.keys()])
 ax.set_xlabel('Fraction of Genes in top CCRs')
 ax.set_xlim(0,1)
 plt.title('Enrichment for Functionally Important Genes in CCRs')
 
-ax.legend((rects3[0], rects1[0], rects5[0], rects2[0]), ('top 5% CCRs', 'top 1% CCRs', 'pLI > 0.9', 'random'),loc='upper right')
+#ax.legend((rects3[0], rects1[0], rects5[0], rects2[0]), ('top 5% CCRs', 'top 1% CCRs', 'pLI > 0.9', 'random'),loc='upper right')
+ax.legend((rects2[0], rects1[0], rects3[0]), ('expected', 'top 1% CCRs', 'pLI > 0.9'),loc='lower right')
+#ax.legend((rects1[0], rects3[0]), ('top 1% CCRs', 'pLI > 0.9'),loc='lower right')
 
 def autolabel(rects1, rects2):
     # attach some text labels
@@ -46,5 +49,5 @@ def autolabel(rects1, rects2):
         ax.text(1.05*width,rect1.get_y() + rect1.get_height()/2.,
                 str(val)+"x", weight='bold')
 
-autolabel(rects1,rects2); autolabel(rects3,rects4); autolabel(rects5,rects6)
+autolabel(rects1,rects2); autolabel(rects3,rects4); #autolabel(rects5,rects6)
 plt.savefig('bars.png', bbox_inches='tight')
