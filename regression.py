69757df214068e7726ddacb40d9300168e238fef
James Havrilla <u1021864@kingspeak23.wasatch.peaks>
2017-02-24 02:56:45 -0700

    added hexbin code

--- regression.py
+++ regression.py
@@ -8,6 +8,7 @@ import seaborn as sns
 import statsmodels.api as sm
 from statsmodels.stats.outliers_influence import OLSInfluence
 import pandas as pd
+from matplotlib.ticker import FormatStrFormatter
 
 import toolshed as ts
 import sys
@@ -40,17 +41,22 @@ intercept=results.params['intercept']; cpgcoef=results.params['CpG']
 fig = plt.figure()
 gs = gridspec.GridSpec(2, 1, height_ratios=[2, 2])
 ax0 = plt.subplot(gs[0])
-ax0.plot(totcpg,totcov,'g.')
-ax0.plot(topcpg,topcov,'b.')
+#ax0.plot(totcpg,totcov,'g.')
+ax0.set_ylim(0,max(totcov))
+g=ax0.hexbin(totcpg, totcov, cmap='rainbow', bins='log')
+ax0.set_xlabel('CpG proportion');ax0.set_ylabel('Sum of coverage fractions')
+cbar = fig.colorbar(g, ax=ax0, orientation='vertical', extend='both', format=FormatStrFormatter('$10^{%.1f}$'))
+#ax0.plot(topcpg,topcov,'b.')
 x=[0,np.max(totcpg)]
 y=[intercept,cpgcoef*np.max(totcpg)]
 ax0.plot(x,y,'r-')
 ax0.set_ylabel('sum(Coverage fractions)')
-ax1 = plt.subplot(gs[1])
-ax1.plot(totcpg,totresid,'r.')
-ax1.plot(topcpg,topresid,'b.')
-ax1.set_xlabel('CpG fraction')
-ax1.set_ylabel('Studentized Residuals')
+#ax1 = plt.subplot(gs[1])
+#ax1.plot(totcpg,totresid,'r.')
+#ax1.plot(topcpg,topresid,'b.')
+#ax1.set_xlabel('CpG fraction')
+#ax1.set_ylabel('Studentized Residuals')
 
-plt.savefig("plots/regression.png")
+plt.tight_layout()
+plt.savefig("plots/regression.pdf", bbox_inches='tight')
 plt.close()
