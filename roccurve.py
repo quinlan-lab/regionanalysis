a9d01f78eb22908e17fc96ef3d2c24d2dbe0384b
Jim Havrilla <u1021864@kingspeak23.wasatch.peaks>
2017-04-11 03:32:48 -0600

    additions to previously simple script to allow optional file input, and filtering on ExAC, HI or AD genes; still in progress

--- roccurve.py
+++ roccurve.py
@@ -4,10 +4,27 @@ from sklearn import metrics
 import matplotlib
 matplotlib.use('Agg')
 from matplotlib import pyplot as plt
+import argparse
 
-title=sys.argv[1]
-p=open('ryan/patho','r')
-b=open('ryan/benign','r')
+parser=argparse.ArgumentParser()
+parser.add_argument("-t", "--title", help="Title")
+parser.add_argument("-c", "--ccr", help="ccr pathogenic percentile file, then benign percentile file", nargs="*")
+parser.set_defaults(ccr = ['tmp/ccrpatho','tmp/ccrbenign'])
+parser.add_argument("-p", "--pli", help="pli pathogenic score file, then benign pli score file", nargs="*")
+parser.set_defaults(ccr = ['tmp/plipatho','tmp/plibenign'])
+parser.add_argument("-d", "--cadd", help="cadd pathogenic score file, then benign cadd score file", nargs="*")
+parser.set_defaults(ccr = ['tmp/caddpatho','tmp/caddbenign'])
+parser.add_argument("-g", "--gnomad", help="ccr pathogenic percentile file, then benign percentile file", nargs="*")
+parser.set_defaults(ccr = ['tmp/ccr2patho','tmp/ccr2benign'])
+args=parser.parse_args()
+ccr=args.ccr
+pli=args.pli
+cadd=args.cadd
+gnomad=args.gnomad
+
+title=args.title
+p=open(ccr[0],'r') #pathogenic percentiles from CCR
+b=open(ccr[1],'r') #benign percentiles from CCR
 y=[]; scores=[]
 for line in p:
     y.append(1)
@@ -18,8 +35,47 @@ for line in b:
 y=np.array(y); scores=np.array(scores)
 fpr, tpr, thresholds = metrics.roc_curve(y, scores, pos_label=1)
 AUC=metrics.roc_auc_score(y, scores)
-plt.plot(fpr,tpr,label='AUC = '+str(AUC))
+plt.plot(fpr,tpr,label='CCR AUC = '+str(AUC), color = 'b')
+p=open(gnomad[0],'r') #pathogenic percentiles from CCR
+b=open(gnomad[1],'r') #benign percentiles from CCR
+y=[]; scores=[]
+for line in p:
+    y.append(1)
+    scores.append(float(line))
+for line in b:
+    y.append(0)
+    scores.append(float(line))
+y=np.array(y); scores=np.array(scores)
+fpr, tpr, thresholds = metrics.roc_curve(y, scores, pos_label=1)
+AUC=metrics.roc_auc_score(y, scores)
+plt.plot(fpr,tpr,label='CCR w/gnomAD AUC = '+str(AUC), color = 'm')
+p=open(pli[0],'r') #pathogenic pLI
+b=open(pli[1],'r') #benign pLI
+y=[]; scores=[]
+for line in p:
+    y.append(1)
+    scores.append(float(line))
+for line in b:
+    y.append(0)
+    scores.append(float(line))
+y=np.array(y); scores=np.array(scores)
+fpr, tpr, thresholds = metrics.roc_curve(y, scores, pos_label=1)
+AUC=metrics.roc_auc_score(y, scores)
+plt.plot(fpr,tpr,label='pLI AUC = '+str(AUC), color = 'g')
+p=open(cadd[0],'r') #pathogenic CADD
+b=open(cadd[1],'r') #benign CADD
+y=[]; scores=[]
+for line in p:
+    y.append(1)
+    scores.append(float(line))
+for line in b:
+    y.append(0)
+    scores.append(float(line))
+y=np.array(y); scores=np.array(scores)
+fpr, tpr, thresholds = metrics.roc_curve(y, scores, pos_label=1)
+AUC=metrics.roc_auc_score(y, scores)
+plt.plot(fpr,tpr,label='CADD AUC = '+str(AUC), color = 'r')
 plt.title(title)
 plt.xlabel('False Positive Rate'); plt.ylabel('True Positive Rate')
-plt.legend(loc='upper left')
-plt.savefig('roc.png',bbox_inches='tight')
+plt.legend(loc='best')
+plt.savefig('roc.pdf',bbox_inches='tight')
