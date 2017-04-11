573e80d09f60954746e2173c009b6b835a49b289
Jim Havrilla <u1021864@kingspeak23.wasatch.peaks>
2017-04-11 03:34:24 -0600

    sweeping changes to pathogenic/benign comparison script for quick runs, allows for different file input, and title input; added a script to intersect CADD rapidly with clinvar

---
+++ caddintersect.py
@@ -0,0 +1,44 @@
+from cyvcf2 import VCF
+import sys
+import argparse
+
+parser=argparse.ArgumentParser()
+parser.add_argument("-p", "--pathogenic", help="pathogenic variant file")
+parser.set_defaults(pathogenic = 'patho.vcf.gz')
+parser.add_argument("-b", "--benign", help="benign variant file")
+parser.set_defaults(benign = 'benign.vcf.gz')
+parser.add_argument("-c", "--cadd", help="cadd file, then cadd indel file")
+parser.set_defaults(cadd = '/scratch/ucgd/lustre/u1021864/serial/CADD.vcf.gz')
+parser.add_argument("-d", "--caddindels", help="cadd indel file")
+parser.set_defaults(caddindels = '/scratch/ucgd/lustre/u1021864/serial/CADDindels.vcf.gz')
+args=parser.parse_args()
+patho=args.pathogenic
+benign=args.benign
+cadd=args.cadd
+caddindels=args.caddindels
+
+cadd = VCF(cadd)
+caddindels = VCF(caddindels)
+patho = VCF(patho)
+benign = VCF(benign)
+
+f1=open('tmp/caddpatho', 'w')
+f2=open('tmp/caddbenign', 'w')
+
+for var in patho:
+    position=var.CHROM+":"+str(var.POS)+"-"+str(var.POS)
+    for v in cadd(position):
+        if v.REF == var.REF and v.ALT[0] == var.ALT[0]: # using v.ID because there is no ID field so ID field is REF and REF is ALT, and var.ALT is already decomposed
+            f1.write(str(v).strip().split('\t')[6]+'\n') # 7th column is always CADD PHRED score
+    for v in caddindels(position):
+        if v.REF == var.REF and v.ALT[0] == var.ALT[0]: # using v.ID because there is no ID field so ID field is REF and REF is ALT, and var.ALT is already decomposed
+            f1.write(str(v).strip().split('\t')[6]+'\n') # 7th column is always CADD PHRED score
+
+for var in benign:
+    position=var.CHROM+":"+str(var.POS)+"-"+str(var.POS)
+    for v in cadd(position):
+        if v.REF == var.REF and v.ALT[0] == var.ALT[0]: # using v.ID because there is no ID field so ID field is REF and REF is ALT
+            f2.write(str(v).strip().split('\t')[6]+'\n') # 7th column is always CADD PHRED score
+    for v in caddindels(position):
+        if v.REF == var.REF and v.ALT[0] == var.ALT[0]: # using v.ID because there is no ID field so ID field is REF and REF is ALT, and var.ALT is already decomposed
+            f2.write(str(v).strip().split('\t')[6]+'\n') # 7th column is always CADD PHRED score
--- pathocompare.sh
+++ pathocompare.sh
@@ -1,3 +1,39 @@
-bedtools intersect -a <(sed '1d' exacresiduals/results/2016_12_10/weightedresiduals.txt) -b patho.vcf | cut -f 12 > ryan/patho
-bedtools intersect -a <(sed '1d' exacresiduals/results/2016_12_10/weightedresiduals.txt) -b benign.vcf | cut -f 12 > ryan/benign
-paste ryan/benign ryan/patho | python ryan/hist.py -o rh.png -t "Clinvar Variant Comparison"
+#for generating ladder plots and ROC curves
+#first run bash clinvarmake.sh if the clinvar file below is not yet generated
+TITLE=$1
+
+python clinvarfilter.py -c $DATA/clinvar-vt-anno-vep.vcf.gz -d genescreens/ad_genecards_clean.txt #-i genescreens/clingen_level3_genes_2015_02_27.tsv # generates the "patho.vcf" and "benign.vcf" files that are strictly filtered based on our criteria
+
+cat <(grep '^#' benign.vcf) <(grep -v '^#' benign.vcf | sort -k1,1 -k2,2n) | bgzip -c > benign.vcf.gz; tabix benign.vcf.gz
+cat <(grep '^#' patho.vcf) <(grep -v '^#' patho.vcf | sort -k1,1 -k2,2n) | bgzip -c > patho.vcf.gz; tabix patho.vcf.gz
+
+#exac
+bedtools intersect -a <(sed '1d' exacresiduals/results/2017_03_30/weightedresiduals-cpg-novariant.txt) -b patho.vcf.gz | cut -f 14 > tmp/ccrpatho
+bedtools intersect -a <(sed '1d' exacresiduals/results/2017_03_30/weightedresiduals-cpg-novariant.txt) -b benign.vcf.gz | cut -f 14 > tmp/ccrbenign
+
+#gnomAD
+bedtools intersect -a <(sed '1d' exacresiduals/results/2017_04_09/weightedresiduals-cpg-novariant.txt) -b patho.vcf.gz | cut -f 14 > tmp/ccr2patho
+bedtools intersect -a <(sed '1d' exacresiduals/results/2017_04_09/weightedresiduals-cpg-novariant.txt) -b benign.vcf.gz | cut -f 14 > tmp/ccr2benign
+
+# generate bed file from pli file (and a vcf CADD file by manually adding a header to the 1.3 version TSV file)
+
+if [ ! -s pli.bed ]; then
+    bedtools groupby -i $DATA/forweb_cleaned_exac_r03_march16_z_data_pLI.txt -g 3,5,6,2 -c 20 -o collapse | tr -s ' ' '\t' > pli.bed
+fi
+if [ ! -s $DATA/CADD.vcf.gz ]; then
+    cat caddheader <(sed '1,2d' $DATA/whole_genome_SNVs.tsv | awk '{print $1,$2,".",$3,$4,$5,$6}' OFS="\t") > $DATA/CADD.vcf # whole_genome_SNVs.tsv is the original CADD file
+    bgzip -@ 12 -c $DATA/CADD.vcf > $DATA/CADD.vcf.gz; tabix $DATA/CADD.vcf.gz
+fi
+
+if [ ! -s $DATA/CADDindels.vcf.gz ]; then
+    cat caddheader <(sed '1,2d' $DATA/InDels.tsv | awk '{print $1,$2,".",$3,$4,$5,$6}' OFS="\t") > $DATA/CADDindels.vcf
+    bgzip -@ 12 -c $DATA/CADDindels.vcf > $DATA/CADDindels.vcf.gz; tabix $DATA/CADDindels.vcf.gz
+fi
+
+bedtools intersect -a <(sed '1d' pli.bed) -b patho.vcf.gz | cut -f 5 > tmp/plipatho
+bedtools intersect -a <(sed '1d' pli.bed) -b benign.vcf.gz | cut -f 5 > tmp/plibenign
+
+python caddintersect.py -c $DATA/CADD.vcf.gz -d $DATA/CADDindels.vcf.gz -p patho.vcf.gz -b benign.vcf.gz 2>/dev/null #2>/dev/null is because the phred score is in the filter column
+
+#paste tmp/ccrbenign tmp/ccrpatho | python ryan/hist.py -o rh.png -t "Clinvar Variant Comparison"
+python roccurve.py -t "$TITLE" -c tmp/ccrpatho tmp/ccrbenign -p tmp/plipatho tmp/plibenign -d tmp/caddpatho tmp/caddbenign -g tmp/ccr2patho tmp/ccr2benign
