1d60b6693619b0da4681576db73cba26ff48206d
Jim Havrilla <u1021864@kingspeak23.wasatch.peaks>
2017-04-11 03:37:04 -0600

    fixes to making exac-annotated clinvar file

--- clinvarmake.sh
+++ clinvarmake.sh
@@ -2,7 +2,7 @@ vt decompose $DATA/clinvar_20170104.vcf.gz -o $DATA/clinvar_20170104_dec.vcf
 
 vt normalize $DATA/clinvar_20170104_dec.vcf -o $DATA/clinvar_vt.vcf -r $DATA/grch37.fa
 
-vcfanno -lua $HOME/software/vcfanno/docs/examples/clinvar_exac.lua -p 4 -base-path $DATA $HOME/software/vcfanno/docs/examples/clinvar_exac.conf $DATA/clinvar_vt.vcf > $DATA/clinvar-anno-vt.vcf
+vcfanno -lua $HOME/software/vcfanno/example/custom.lua -p 4 -base-path $DATA clinvar_exac.conf $DATA/clinvar_vt.vcf > $DATA/clinvar-anno-vt.vcf
 
 perl $HOME/software/ensembl-tools-release-81/scripts/variant_effect_predictor/variant_effect_predictor.pl -i $DATA/clinvar-anno-vt.vcf --cache --sift b --polyphen b --symbol --numbers --biotype --total_length --allele_number -o $DATA/clinvar-vt-anno-vep.vcf --vcf --fields Consequence,Codons,Amino_acids,Gene,SYMBOL,Feature,EXON,PolyPhen,SIFT,Protein_position,BIOTYPE,ALLELE_NUM --offline
 
