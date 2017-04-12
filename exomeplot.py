6a45189d5bfd70bcd10d58ee8f6469535019e8d3
Jim Havrilla <u1021864@kingspeak23.wasatch.peaks>
2017-04-12 14:40:12 -0600

    minor changes to exomeplot

--- exomeplot.py
+++ exomeplot.py
@@ -8,8 +8,18 @@ import seaborn as sns
 import numpy as np
 import toolshed as ts
 
+import argparse
+
+parser=argparse.ArgumentParser()
+parser.add_argument("-f", "--file", help="regions file for analysis", required=True)
+parser.add_argument("-p", "--percentiletest", action="store_true", help="plots percentile distribution plot")
+#parser.set_defaults(file="$HOME/analysis/exacresiduals/results/2017_04_09/weightedresiduals-cpg-novariant.txt")
+args=parser.parse_args()
+infile=args.file
+percentiletest=args.percentiletest
+
 lens=[]
-it = ts.reader('$HOME/analysis/exacresiduals/results/2016_12_10/unfilteredresiduals.txt')
+it = ts.reader(infile)
 iterable = (i for i in it)
 for region in iterable:
     lens.append(int(region['end'])-int(region['start']))
@@ -33,25 +43,26 @@ ax1.set_ylabel('Proportion of regions represented')
 ax1.set_xlabel('Distance between functional variants')
 plt.savefig('funchist.pdf', format='pdf', bbox_inches='tight')
 
-totlen=0.0; ordered=[]; exomecov=[]; wgtdpct=[]
-it = ts.reader('$HOME/analysis/exacresiduals/results/2016_12_10/weightedresiduals.txt')
-iterable = (i for i in it)
-for region in iterable:
-    length=int(region['end'])-int(region['start'])
-    totlen+=length
-    ordered.append((length,float(region['weighted_pct']),region['ranges']+region['gene']))
-ordered=sorted(ordered,key=lambda x:x[1])
-length=0.0
-prevregion=None
-for region in ordered:
-    length+=region[0]
-    if ordered[2]!=prevregion and prevregion!=None:
-        exomecov.append(prevvalues[0])
-        wgtdpct.append(prevvalues[1])
-    prevregion=region[2]
-    prevvalues=(length/totlen,region[1])
-plt.figure()
-plt.plot(wgtdpct,exomecov)
-plt.xlabel('CCR Percentile (highest = most constrained)')
-plt.ylabel('Proportion of exome covered in total')
-plt.savefig('exomecov.png',bbox_inches='tight')
+if percentiletest:
+    totlen=0.0; ordered=[]; exomecov=[]; wgtdpct=[]
+    it = ts.reader(infile)
+    iterable = (i for i in it)
+    for region in iterable:
+        length=int(region['end'])-int(region['start'])
+        totlen+=length
+        ordered.append((length,float(region['weighted_pct']),region['ranges']+region['gene']))
+    ordered=sorted(ordered,key=lambda x:x[1])
+    length=0.0
+    prevregion=None
+    for region in ordered:
+        length+=region[0]
+        if ordered[2]!=prevregion and prevregion!=None:
+            exomecov.append(prevvalues[0])
+            wgtdpct.append(prevvalues[1])
+        prevregion=region[2]
+        prevvalues=(length/totlen,region[1])
+    plt.figure()
+    plt.plot(wgtdpct,exomecov)
+    plt.xlabel('CCR Percentile (highest = most constrained)')
+    plt.ylabel('Proportion of exome covered in total')
+    plt.savefig('exomecov.png',bbox_inches='tight')
