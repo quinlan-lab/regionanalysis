87244f2e90cb02e5021b08e7970377b3592c9b6a
Jim Havrilla <u1021864@kingspeak23.wasatch.peaks>
2017-04-29 19:45:34 -0600

    adding csq strings from exac and gnomad, allowing overlaps that are not allele specific

--- varmake.sh
+++ varmake.sh
@@ -10,7 +10,7 @@ if [ ! -s $DATA/${FILE}_vt.vcf ]; then
     vt normalize $DATA/${FILE}_dec.vcf -o $DATA/${FILE}_vt.vcf -r $DATA/grch37.fa
 fi
 
-vcfanno -lua custom.lua -p 4 -base-path $DATA annovars.conf $DATA/${FILE}_vt.vcf > $DATA/${FILE}-anno-vt.vcf
+vcfanno -lua custom.lua -permissive-overlap -p 4 -base-path $DATA annovars.conf $DATA/${FILE}_vt.vcf > $DATA/${FILE}-anno-vt.vcf
 
 perl $HOME/software/ensembl-tools-release-81/scripts/variant_effect_predictor/variant_effect_predictor.pl -i $DATA/${FILE}-anno-vt.vcf --cache --sift b --polyphen b --symbol --numbers --biotype --total_length --allele_number -o $DATA/${FILE}-vep-anno-vt.vcf --vcf --fields Consequence,Codons,Amino_acids,Gene,SYMBOL,Feature,EXON,PolyPhen,SIFT,Protein_position,BIOTYPE,ALLELE_NUM --offline --fork 12 --force_overwrite
 
