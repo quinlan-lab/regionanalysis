eb642d4fa8301f297ca8ce820fd2c254cdfa6ab8
Jim Havrilla <u1021864@kingspeak23.wasatch.peaks>
2017-05-01 02:42:58 -0600

    consistent VEP annotation format

--- varmake.sh
+++ varmake.sh
@@ -16,6 +16,6 @@ if [ $exac ]; then
 fi
 vcfanno -lua custom.lua -permissive-overlap -p 4 -base-path $DATA annovars.conf $DATA/${FILE}_vt.vcf > $DATA/${FILE}-anno-vt.vcf
 
-perl $HOME/software/ensembl-tools-release-81/scripts/variant_effect_predictor/variant_effect_predictor.pl -i $DATA/${FILE}-anno-vt.vcf --cache --sift b --polyphen b --symbol --numbers --biotype --total_length --allele_number -o $DATA/${FILE}-vep-anno-vt.vcf --vcf --fields Consequence,Codons,Amino_acids,Gene,SYMBOL,Feature,EXON,PolyPhen,SIFT,Protein_position,BIOTYPE,ALLELE_NUM --offline --fork 12 --force_overwrite
+perl $HOME/software/ensembl-tools-release-81/scripts/variant_effect_predictor/variant_effect_predictor.pl -i $DATA/${FILE}-anno-vt.vcf --cache --sift b --polyphen b --symbol --numbers --biotype --total_length --allele_number -o $DATA/${FILE}-vep-anno-vt.vcf --vcf --fields ALLELE,Consequence,Codons,Amino_acids,Gene,SYMBOL,Feature,EXON,PolyPhen,SIFT,Protein_position,BIOTYPE,ALLELE_NUM --offline --fork 12 --force_overwrite
 
 bgzip -c $DATA/${FILE}-vep-anno-vt.vcf > $DATA/${FILE}-vep-anno-vt.vcf.gz; tabix $DATA/${FILE}-vep-anno-vt.vcf.gz
