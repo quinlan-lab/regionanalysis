cdd697ee17f9ade6a0f50f7e89b64a4f9923809a
Jim Havrilla <u1021864@kingspeak23.wasatch.peaks>
2017-05-08 17:24:38 -0600

    sweeping changes to incorporate new code for filtering and removal of vcfanno

--- pathocompare.sh
+++ pathocompare.sh
@@ -1,15 +1,36 @@
 #for generating ladder plots and ROC curves
 #first run bash clinvarmake.sh if the clinvar file below is not yet generated
 
-if [ ! -s $DATA/clinvar_20170104-vep-anno-vt.vcf.gz ]; then
+if [ ! -s $DATA/clinvar_20170104-vep-vt.vcf.gz ]; then
     bash varmake.sh $DATA/clinvar_20170104.vcf.gz
 fi
 
-if [ ! -s $DATA/gnomad-vep-anno-vt.vcf.gz ]; then
+if [ ! -s $DATA/gnomad-vep-vt.vcf.gz ]; then
     bash varmake.sh $DATA/gnomad.exomes.r2.0.1.sites.vcf.gz exac # doesn't annotate gnomad with gnomad
-
-#    bedtools intersect -v -a $DATA/gnomad-vep-anno-vt.vcf.gz -b $DATA/ExAC.r1.vt.vep.vcf.gz > $DATA/gnomad-noexac.vcf; bgzip $DATA/gnomad-noexac.vcf -c > $DATA/gnomad-noexac.vcf.gz; tabix $DATA/gnomad-noexac.vcf.gz
 fi
+
+if [ ! -s $DATA/clinvar-exac.txt ] | [ ! -s $DATA/clinvar-gnomad.txt ] ; then
+    bedtools intersect -a $DATA/clinvar_20170104-vep-vt.vcf.gz -b $DATA/gnomad-vep-vt.vcf.gz -wa -wb > $DATA/clinvar-gnomad.txt
+    bedtools intersect -a $DATA/clinvar_20170104-vep-vt.vcf.gz -b $DATA/ExAC.r1.vt.vep.vcf.gz -wa -wb > $DATA/clinvar-exac.txt
+fi
+
+if [ ! -s $DATA/gnomad-exac.txt ]; then
+    bedtools intersect -a $DATA/gnomad-vep-vt.vcf.gz -b $DATA/ExAC.r1.vt.vep.vcf.gz -wa -wb > $DATA/gnomad-exac.txt
+fi
+
+python parvarfilter.py -x $DATA/clinvar-gnomad.txt -f -n clinvar -c -s patho -e gnomad
+cat <(zgrep "^#" $DATA/clinvar_20170104.vcf.gz ) <(sort -k1,1 -k2,2n $DATA/clinvar-patho-gnomad.txt | uniq) > $DATA/clinvar-patho-gnomad.vcf
+python parvarfilter.py -x $DATA/clinvar-exac.txt -f -n clinvar -c -s patho -e exac
+cat <(zgrep "^#" $DATA/clinvar_20170104.vcf.gz ) <(sort -k1,1 -k2,2n $DATA/clinvar-patho-exac.txt | uniq) > $DATA/clinvar-patho-exac.vcf
+python parvarfilter.py -x $DATA/clinvar-gnomad.txt -f -n clinvar -c -s benign -e gnomad
+cat <(zgrep "^#" $DATA/clinvar_20170104.vcf.gz ) <(sort -k1,1 -k2,2n $DATA/clinvar-benign-gnomad.txt | uniq) > $DATA/clinvar-benign-gnomad.vcf
+#python varfilter.py -x $DATA/clinvar_20170104-vep-anno-vt.vcf.gz -e exac -d genescreens/ad_genecards_clean.txt -c -f -n clinvar -s patho #-i genescreens/clingen_level3_genes_2015_02_27.tsv # generates the "patho.vcf" and "benign.vcf" files that are strictly filtered based on our criteria
+#python varfilter.py -x $DATA/clinvar_20170104-vep-anno-vt.vcf.gz -e gnomad -d genescreens/ad_genecards_clean.txt -c -f -n clinvar -s patho #-i genescreens/clingen_level3_genes_2015_02_27.tsv # generates the "patho.vcf" and "benign.vcf" files that are strictly filtered based on our criteria
+#python varfilter.py -x $DATA/clinvar_20170104-vep-anno-vt.vcf.gz -e gnomad -d genescreens/ad_genecards_clean.txt -c -f -n clinvar -s benign #-i genescreens/clingen_level3_genes_2015_02_27.tsv # generates the "patho.vcf" and "benign.vcf" files that are strictly filtered based on our criteria
+
+EP=$(grep -v "^#" $DATA/clinvar-patho-exac.vcf | wc -l)
+GP=$(grep -v "^#" $DATA/clinvar-patho-gnomad.vcf | wc -l)
+GB=$(grep -v "^#" $DATA/clinvar-benign-gnomad.vcf | wc -l)
 
 while getopts ":t:gc" opt; do
     case $opt in
@@ -18,10 +39,13 @@ while getopts ":t:gc" opt; do
             ;;
         g)
             echo "-gnomad as benign input was triggered" >&2
-            python varfilter.py -x $DATA/gnomad-vep-anno-vt.vcf.gz -e exac -f -n gnomad -s benign -d genescreens/ad_genecards_clean.txt #ogfiles/all_ad.tsv # -d genescreens/ad_genecards_clean.txt #-i genescreens/clingen_level3_genes_2015_02_27.tsv # generates the "patho.vcf" and "benign.vcf" files that are strictly filtered based on our criteria
+            python parvarfilter.py -x $DATA/gnomad-exac.txt -f -n gnomad -c -s benign -e exac
+            cat <(zgrep "^#" $DATA/gnomad.exomes.r2.0.1.sites.vcf.gz ) <(sort -k1,1 -k2,2n $DATA/gnomad-benign-exac.txt | uniq) > $DATA/gnomad-benign-exac.vcf
+            #python varfilter.py -x $DATA/gnomad-vep-anno-vt.vcf.gz -e exac -f -n gnomad -s benign -d genescreens/ad_genecards_clean.txt #ogfiles/all_ad.tsv # -d genescreens/ad_genecards_clean.txt #-i genescreens/clingen_level3_genes_2015_02_27.tsv # generates the "patho.vcf" and "benign.vcf" files that are strictly filtered based on our criteria
 
             #exac
-            cat <(grep '^#' $DATA/gnomad-benign-exac.vcf) <(grep -v '^#' $DATA/gnomad-benign-exac.vcf | sort -k1,1 -k2,2n) | bgzip -c > $DATA/gnomad-benign-exac.vcf.gz; tabix $DATA/gnomad-benign-exac.vcf.gz
+            cat <(grep '^#' $DATA/gnomad-benign-exac.vcf) <(grep -v '^#' $DATA/gnomad-benign-exac.vcf | shuf -n $EP | sort -k1,1 -k2,2n) | bgzip -c > $DATA/gnomad-benign-exac.vcf.gz; tabix $DATA/gnomad-benign-exac.vcf.gz # shuf adds subsampling
+            EB=$(zgrep -v "^#" $DATA/gnomad-benign-exac.vcf.gz | wc -l)
             #bedtools intersect -a $DATA/gnomad-benign-exac.vcf.gz -b $DATA/ExAC.r1.vt.vep.vcf.gz -v > $DATA/gnomadbenigns.vcf.gz
             bedtools intersect -a <(sed '1d' exacresiduals/results/exacv1newweight/weightedresiduals-cpg-novariant.txt) -b $DATA/gnomad-benign-exac.vcf.gz | cut -f 14 > tmp/ccrbenign
             python caddintersect.py -c $DATA/CADD.vcf.gz -d $DATA/CADDindels.vcf.gz -p $DATA/clinvar-patho-exac.vcf.gz -b $DATA/gnomad-benign-exac.vcf.gz -f tmp/caddpatho tmp/caddbenign 2>/dev/null #2>/dev/null is because the phred score is in the filter column
@@ -30,7 +54,10 @@ while getopts ":t:gc" opt; do
             ;;
         c)
             echo "-clinvar input triggered" >&2
-            python varfilter.py -x $DATA/clinvar_20170104-vep-anno-vt.vcf.gz -e exac -d genescreens/ad_genecards_clean.txt -c -f -n clinvar -s benign #-i genescreens/clingen_level3_genes_2015_02_27.tsv # generates the "patho.vcf" and "benign.vcf" files that are strictly filtered based on our criteria
+            python parvarfilter.py -x $DATA/clinvar-exac.txt -f -n clinvar -c -s benign -e exac
+            cat <(zgrep "^#" $DATA/clinvar_20170104.vcf.gz ) <(sort -k1,1 -k2,2n $DATA/clinvar-benign-exac.txt | uniq) > $DATA/clinvar-benign-exac.vcf
+            #python varfilter.py -x $DATA/clinvar_20170104-vep-anno-vt.vcf.gz -e exac -d genescreens/ad_genecards_clean.txt -c -f -n clinvar -s benign #-i genescreens/clingen_level3_genes_2015_02_27.tsv # generates the "patho.vcf" and "benign.vcf" files that are strictly filtered based on our criteria
+            EB=$(grep -v "^#" $DATA/clinvar-benign-exac.vcf | wc -l)
 
             #exac
             cat <(grep '^#' $DATA/clinvar-benign-exac.vcf) <(grep -v '^#' $DATA/clinvar-benign-exac.vcf | sort -k1,1 -k2,2n) | bgzip -c > $DATA/clinvar-benign-exac.vcf.gz; tabix $DATA/clinvar-benign-exac.vcf.gz
@@ -51,11 +78,6 @@ while getopts ":t:gc" opt; do
 done
 
 echo "-title input: '${TITLE[@]}'" >&2
-
-python varfilter.py -x $DATA/clinvar_20170104-vep-anno-vt.vcf.gz -e exac -d genescreens/ad_genecards_clean.txt -c -f -n clinvar -s patho #-i genescreens/clingen_level3_genes_2015_02_27.tsv # generates the "patho.vcf" and "benign.vcf" files that are strictly filtered based on our criteria
-python varfilter.py -x $DATA/clinvar_20170104-vep-anno-vt.vcf.gz -e gnomad -d genescreens/ad_genecards_clean.txt -c -f -n clinvar -s patho #-i genescreens/clingen_level3_genes_2015_02_27.tsv # generates the "patho.vcf" and "benign.vcf" files that are strictly filtered based on our criteria
-python varfilter.py -x $DATA/clinvar_20170104-vep-anno-vt.vcf.gz -e gnomad -d genescreens/ad_genecards_clean.txt -c -f -n clinvar -s benign #-i genescreens/clingen_level3_genes_2015_02_27.tsv # generates the "patho.vcf" and "benign.vcf" files that are strictly filtered based on our criteria
-# generate bed file from pli file (and a vcf CADD file by manually adding a header to the 1.3 version TSV file)
 
 #exac
 cat <(grep '^#' $DATA/clinvar-patho-exac.vcf) <(grep -v '^#' $DATA/clinvar-patho-exac.vcf | sort -k1,1 -k2,2n) | bgzip -c > $DATA/clinvar-patho-exac.vcf.gz; tabix $DATA/clinvar-patho-exac.vcf.gz
@@ -115,7 +137,11 @@ python caddintersect.py -c $DATA/CADD.vcf.gz -d $DATA/CADDindels.vcf.gz -p $DATA
 
 paste tmp/mcraepatho | python hist.py -o mcraeexac_dist.pdf #-t "$TITLE"
 paste tmp/mcrae2patho | python hist.py -o mcraegnomad_dist.pdf #-t "$TITLE"
-paste tmp/ccrbenign tmp/ccrpatho | python hist.py -o exac_dist.pdf -t "$TITLE"
-paste tmp/ccr2benign tmp/ccr2patho | python hist.py -o gnomad_dist.pdf -t "$TITLE"
-python roccurve.py -t "${TITLE[0]}" -c tmp/ccrpatho tmp/ccrbenign -p tmp/plipatho tmp/plibenign -d tmp/caddpatho tmp/caddbenign -r tmp/rvispatho tmp/rvisbenign -o exac_roc.pdf
-python roccurve.py -t "${TITLE[1]}" -g tmp/ccr2patho tmp/ccr2benign -p tmp/pli2patho tmp/pli2benign -d tmp/cadd2patho tmp/cadd2benign -r tmp/rvis2patho tmp/rvis2benign -o gnomad_roc.pdf
+paste tmp/ccrbenign tmp/ccrpatho | python hist.py -o exac_dist.pdf -t "${TITLE[0]}"
+paste tmp/ccr2benign tmp/ccr2patho | python hist.py -o gnomad_dist.pdf -t "${TITLE[1]}"
+python roccurve.py -t "${TITLE[0]}" -c tmp/ccrpatho tmp/ccrbenign -p tmp/plipatho tmp/plibenign -d tmp/caddpatho tmp/caddbenign -r tmp/rvispatho tmp/rvisbenign -o exac_roc.pdf -n $EP $EB
+python roccurve.py -t "${TITLE[1]}" -g tmp/ccr2patho tmp/ccr2benign -p tmp/pli2patho tmp/pli2benign -d tmp/cadd2patho tmp/cadd2benign -r tmp/rvis2patho tmp/rvis2benign -o gnomad_roc.pdf -n $GP $GB
+
+if [ ! -s $DATA/newexac.vcf ]; then
+    bedtools intersect -a $DATA/CADD.vcf.gz -b <(sed '1d' exacresiduals/results/exacv1newweight/weightedresiduals-cpg-novariant.txt) -wb | awk '{print $1,$2,$3,$4,$5,$6,$7+-10*log(1-$21/100)/log(10)}' FS='\t' OFS='\t' > $DATA/newexac.vcf
+fi
--- varmake.sh
+++ varmake.sh
@@ -1,4 +1,3 @@
-exac=$2 # if exaconly, put something here
 original=$1
 fileName="${original##*/}"
 fileExt=${fileName#*.}
@@ -11,22 +10,6 @@ if [ ! -s $DATA/${FILE}_vt.vcf ]; then
     vt normalize $DATA/${FILE}_dec.vcf -o $DATA/${FILE}_vt.vcf -r $DATA/grch37.fa
 fi
 
-if [ ! -s $DATA/ExAC.r1.vt.vep.alts.vcf.gz ]; then
-    cat <(grep "^#" $DATA/ExAC.r1.vt.vep.vcf) <(grep -v "^#" $DATA/ExAC.r1.vt.vep.vcf | awk '{print $0 ";refs=" $4 ";" "alts=" $5}' FS='\t' OFS='\t') > $DATA/ExAC.r1.vt.vep.alts.vcf; bgzip -c $DATA/ExAC.r1.vt.vep.alts.vcf > $DATA/ExAC.r1.vt.vep.alts.vcf.gz; tabix $DATA/ExAC.r1.vt.vep.alts.vcf.gz
-fi
+perl $HOME/software/ensembl-tools-release-81/scripts/variant_effect_predictor/variant_effect_predictor.pl -i $DATA/${FILE}_vt.vcf --cache --sift b --polyphen b --symbol --numbers --biotype --total_length --allele_number -o $DATA/${FILE}-vep-vt.vcf --vcf --fields ALLELE,Consequence,Codons,Amino_acids,Gene,SYMBOL,Feature,EXON,PolyPhen,SIFT,Protein_position,BIOTYPE,ALLELE_NUM,cDNA_position --offline --fork 12 --force_overwrite
 
-if [ ! -s $DATA/gnomad.exomes.r2.0.1.sites.vep.vt.alts.vcf.gz ]; then
-    cat <(grep "^#" $DATA/gnomad.exomes.r2.0.1.sites.vep.vt.vcf) <(grep -v "^#" $DATA/gnomad.exomes.r2.0.1.sites.vep.vt.vcf | awk '{print $0 ";refs=" $4 ";" "alts=" $5}' FS='\t' OFS='\t') > $DATA/gnomad.exomes.r2.0.1.sites.vep.vt.alts.vcf; bgzip -c $DATA/gnomad.exomes.r2.0.1.sites.vep.vt.alts.vcf > $DATA/gnomad.exomes.r2.0.1.sites.vep.vt.alts.vcf.gz; tabix $DATA/gnomad.exomes.r2.0.1.sites.vep.vt.alts.vcf.gz
-fi
-
-# cat <(grep "^#" $DATA/gnomad_vt.vcf) <(grep -v "^#" $DATA/gnomad_vt.vcf | awk '{print $0 ";refs=" $4 ";" "alts=" $5}' FS='\t' OFS='\t') command to add refs and alts here, then add gnomad_alts from alts and exac_alts from alts to conf files depending on file annotated with
-
-if [ $exac ]; then
-    vcfanno -lua $HOME/analysis/custom.lua -permissive-overlap -p 4 -base-path $DATA $HOME/analysis/exaconly.conf $DATA/${FILE}_vt.vcf > $DATA/${FILE}-anno-vt.vcf
-else
-    vcfanno -lua $HOME/analysis/custom.lua -permissive-overlap -p 4 -base-path $DATA $HOME/analysis/annovars.conf $DATA/${FILE}_vt.vcf > $DATA/${FILE}-anno-vt.vcf
-fi
-
-perl $HOME/software/ensembl-tools-release-81/scripts/variant_effect_predictor/variant_effect_predictor.pl -i $DATA/${FILE}-anno-vt.vcf --cache --sift b --polyphen b --symbol --numbers --biotype --total_length --allele_number -o $DATA/${FILE}-vep-anno-vt.vcf --vcf --fields ALLELE,Consequence,Codons,Amino_acids,Gene,SYMBOL,Feature,EXON,PolyPhen,SIFT,Protein_position,BIOTYPE,ALLELE_NUM,cDNA_position --offline --fork 12 --force_overwrite
-
-bgzip -c $DATA/${FILE}-vep-anno-vt.vcf > $DATA/${FILE}-vep-anno-vt.vcf.gz; tabix $DATA/${FILE}-vep-anno-vt.vcf.gz
+bgzip -c $DATA/${FILE}-vep-vt.vcf > $DATA/${FILE}-vep-vt.vcf.gz; tabix $DATA/${FILE}-vep-vt.vcf.gz
