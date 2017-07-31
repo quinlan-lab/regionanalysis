bf5e54cb3621114460ba430c6f4913f3e468d4a8
Jim Havrilla <u1021864@kingspeak23.wasatch.peaks>
2017-07-31 03:38:36 -0600

    new code for running another gene and an output file to html page

--- runproplot.sh
+++ runproplot.sh
@@ -1,3 +1,4 @@
 DATA=/scratch/ucgd/lustre/u1021864/serial
 HOME=/uufs/chpc.utah.edu/common/home/u1021864
-python proplot.py -t ENST00000359125 -b gnomad-ccrs.bed.gz -p $HOME/software/pathoscore/truth-sets/GRCh37/clinvar/clinvar-pathogenic-likely_pathogenic.20170710.vcf.gz -c essentials/gnomad.vcf.gz -g $DATA/Homo_sapiens.GRCh37.75.gtf.gz --region 20:62037542-62103993 --pfam $DATA/pfam.bed.gz
+python proplot.py -t ENST00000359125 -b exacresiduals/gnomad10x.5-ccrs.bed.gz -p $HOME/software/pathoscore/truth-sets/GRCh37/clinvar/clinvar-pathogenic-likely_pathogenic.20170710.vcf.gz -c essentials/gnomad.vcf.gz -g $DATA/Homo_sapiens.GRCh37.75.gtf.gz --region 20:62037542-62103993 --pfam $DATA/pfam.bed.gz -f KCNQ2
+python proplot.py -t ENST00000245503 -b exacresiduals/gnomad10x.5-ccrs.bed.gz -p $HOME/software/pathoscore/truth-sets/GRCh37/clinvar/clinvar-pathogenic-likely_pathogenic.20170710.vcf.gz -c essentials/gnomad.vcf.gz -g $DATA/Homo_sapiens.GRCh37.75.gtf.gz --region 17:10424600-10451237 --pfam $DATA/pfam.bed.gz -f MYH2
