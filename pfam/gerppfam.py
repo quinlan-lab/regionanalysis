2bb0121b73dbf3ab49621e6ebb184a0a7938ed96
Jim Havrilla <u1021864@kingspeak23.wasatch.peaks>
2017-08-03 18:47:43 -0600

    gets gerp means and ccr means for pfams now; small change to exonicpfam file

--- pfam/gerppfam.py
+++ pfam/gerppfam.py
@@ -2,32 +2,50 @@ from bw import BigWig
 import numpy as np
 import tabix 
 from operator import itemgetter
+import subprocess as sp
 
-def score_average(pfams,ccrs):
-    varprev = None; varprevscore = 0.0
-    for variant in intersect(variants, ccr).split("\n"):
-        fields = variant.strip().split("\t")
-        var = [fields[x] for x in [0,1,3,4]]
-        overlap = int(fields[-1]) + 1 # until aaron fixes bedtools off-by-one bug, need to add +1
+def intersect(variants, regions, wo = True, sortedbool=False):
+    def killproc(p):
+        try:
+            p.kill()
+        except OSError:
+            pass
+    l = ['bedtools', 'intersect', '-a', variants, '-b', regions]
+    if wo:
+        l.append('-wo')
+    if sortedbool:
+        l.append('-sorted')
+    p1 = sp.Popen(l, stdout = sp.PIPE)
+    output,error = p1.communicate()
+    killproc(p1)
+    return output.strip()
+
+def score_average(pfams,ccr):
+    famprev = None
+    lengths=[]; scores=[]; ccrdict={}
+    for isxn in intersect(pfams, ccr).split("\n"):
+        fields = isxn.strip().split("\t")
+        fam = fields[3]
+        overlap = float(fields[-1]) # need to add ccr*overlap/total overlap?`
         ccrscore = float(fields[-2])
-        if varprev is None:
+        if famprev is None:
             pass # will be dealt with at next pass through loop
-        elif varprev == var:  
-            varprevscore+=overlap
-        elif varprev != var:
-            if varprevscore == 0.0:
-                if average:
-                    varprevscore += ccrprev * overprev / float(len(varprev[2]))
-            print "%.3f" % varprevscore
-            varprevscore = 0.0
-        varprev = var; overprev = overlap; ccrprev = ccrscore
+        elif famprev == fam:  
+            lengths.append(overprev)
+            scores.append(ccrprev)
+        elif famprev != fam:
+            lengths.append(overprev)
+            scores.append(ccrprev)
+            famprevscore=sum([a*b for a,b in zip(scores,lengths)])/sum(lengths)
+            ccrdict[famprev]=famprevscore
+            lengths=[]; scores=[]
+        famprev = fam; overprev = overlap; ccrprev = ccrscore
 
-    if varprevscore == 0.0:
-        if average:
-            varprevscore += ccrprev * overprev / float(len(varprev[2]))
-    print "%.3f" % varprevscore
+    famprevscore=sum([a*b for a,b in zip(scores,lengths)])/sum(lengths)
+    return ccrdict
 
 def read_ccrs(ccr, pfam):
+    ccrs=[]
     chrom = pfam[0]
     if "chr" in chrom:
         chrom=chrom.split("chr")[1]
@@ -35,20 +53,19 @@ def read_ccrs(ccr, pfam):
     region=chrom+":"+start+"-"+end
     try:
         for r in ccr.querys(region):
-            pfam
+            ccrs.append((r[0],int(r[1]),int(r[2]),r[-1]))
     except:
         pass  #just because chrom isn't present in a file, pytabix will throw a "TabixError"
-
-    return 0
+    
+    return ccrs
 
 def read_gerp(gerp, pfam):
     chrom = pfam[0]
     if "chr" not in chrom:
         chrom="chr"+chrom
     start=pfam[1]; end=pfam[2]; fam=pfam[3]
-    #print fam, np.nanmean(np.frombuffer(gerp.values(chrom, start, end), dtype='f'))
-
-    return 0
+    gerps=np.frombuffer(gerp.values(chrom, start, end), dtype='f')
+    return fam, np.nanmean(gerps), len(gerps)
 
 def read_pfam(pfam):
     pfam=open(pfam,'r')
@@ -62,14 +79,43 @@ def read_pfam(pfam):
     pfams.sort(key=itemgetter(3)) # much faster than lambda tup: tup[3]
     return pfams
 
+def score_gerp(pfams, gerp):
+    gerpdict={}; lengths=[]; scores=[]; famprev = None
+    for pfam in pfams:
+        pfam, gerpscore, overlap = read_gerp(gerp, pfam) # _ = pfam, redundant variable
+        if famprev is None:
+            pass # will be dealt with at next pass through loop
+        elif famprev == pfam:  
+            lengths.append(overprev)
+            scores.append(gerpprev)
+        elif famprev != pfam:
+            lengths.append(overprev)
+            scores.append(gerpprev)
+            famprevscore=sum([a*b for a,b in zip(scores,lengths)])/sum(lengths)
+            gerpdict[famprev]=famprevscore
+            lengths=[]; scores=[]
+        famprev = pfam; overprev = overlap; gerpprev = gerpscore
+    
+    lengths.append(overprev) 
+    scores.append(gerpprev)
+    famprevscore=sum([a*b for a,b in zip(scores,lengths)])/sum(lengths)
+    gerpdict[famprev]=famprevscore
+    return gerpdict
+
 #pfampath='pfam.hg19.bed' # pfam doms incl. introns
-pfampath='pfam.browser.bed' # pfam doms by exon
+pfampath='/uufs/chpc.utah.edu/common/home/u1021864/analysis/pfam/pfam.exonic.bed' # pfam doms by exon; sorted by pfam name, in pfamconvert.sh
 gerppath='/scratch/ucgd/lustre/u1021864/serial/hg19.gerp.bw'
 ccrpath='/uufs/chpc.utah.edu/common/home/u1021864/analysis/exacresiduals/gnomad10x.5-ccrs.bed.gz'
 
 gerp = BigWig(gerppath)
-pfams=read_pfam(pfampath)
-ccr = tabix.open(ccrpath)
-for pfam in pfams:
-    read_ccrs(ccr, pfam)
-    read_gerp(gerp, pfam)
+pfams = read_pfam(pfampath)
+#ccr = tabix.open(ccrpath)
+
+ccrs=score_average(pfampath, ccrpath)
+gerps=score_gerp(pfams, gerp)
+    
+for i in ccrs:
+    print i, ccrs[i]
+for i in gerps:
+    print i, gerps[i]
+#for pfam in ccrs:
