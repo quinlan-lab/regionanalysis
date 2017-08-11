af64e1bc903280814e1680b12e939a55ccfc150e
Jim Havrilla <u1021864@kingspeak23.wasatch.peaks>
2017-08-11 14:35:38 -0600

    cleaned up pfam figure, using new pfam definitions from ucsc, and full family names where applicable

--- pfam/pfamvccr.sh
+++ pfam/pfamvccr.sh
@@ -1,15 +1,19 @@
 #mysql --user=genome --host=genome-mysql.soe.ucsc.edu -D hg19 -A -e  'SELECT chrom,chromStart,chromEnd,name from ucscGenePfam;' | perl -pe 's/^chr//' | bgzip -c > pfam.hg19.bed.gz
-if [ ! -s pfamflat.bed ]; then
-    python flattenpfams.py $DATA/pfam.bed > pfamflat.bed # flatten exomes across transcripts
+if [ ! -s pfam.exonic.bed ]; then
+    python flattenpfams.py $DATA/pfam.bed > pfam.exonic.bed # flatten exomes across transcripts
 fi
-bedtools intersect -a ../exacresiduals/gnomad10x.5-ccrs.bed.gz -b pfamflat.bed -wao -sorted > ccr-pfam.bed # pfam.bed is GRCh37.gtf with intron-containing pfam file across chrom 1-22, X, Y;
+if [ ! -s Pfam-A.clans.tsv ]; then
+    wget ftp://ftp.ebi.ac.uk/pub/databases/Pfam/current_release/Pfam-A.clans.tsv.gz
+    gunzip Pfam-A.clans.tsv.gz
+fi
+bedtools intersect -a ../exacresiduals/gnomad10x.5-ccrs.bed.gz -b pfam.exonic.bed -wao > ccr-pfam.bed # pfam.bed is GRCh37.gtf with intron-containing pfam file across chrom 1-22, X, Y;
 cat ccr-pfam.bed | python ccrpfam.py
 paste tmp/nodomccrs tmp/domccrs | python ../hist.py -o pfam_dist.pdf -t "gnomad based CCR v Pfam" -l "nodoms" "doms" # gnomad based CCR
 awk '$14>90' ccr-pfam.bed | cut -f 18 | sort | uniq -c | sort -k1,1nr | head -100 | sed '1d' | sed 's/^\s*\S*//g' > top100doms #top 100 domains
-bedtools intersect -b ../exacresiduals/gnomad10x.5-ccrs.bed.gz -a pfamflat.bed -u -sorted | cut -f 4 | sort | uniq > pfams.txt
+bedtools intersect -b ../exacresiduals/gnomad10x.5-ccrs.bed.gz -a pfam.exonic.bed -u | cut -f 4 | sort | uniq > pfams.txt
 #5117/5416 uniq pfams covered by gnomad-ccrs.bed.gz
-bedtools intersect -a pfamflat.bed -b ../exacresiduals/gnomad10x.5-ccrs.bed.gz -wb -sorted | sort -k4,4 > pfam-ccr.bed # pfam.bed is GRCh37.gtf with intron-containing pfam file across chrom 1-22, X, Y;
+bedtools intersect -a pfam.exonic.bed -b ../exacresiduals/gnomad10x.5-ccrs.bed.gz -wb | sort -k4,4 > pfam-ccr.bed # pfam.bed is GRCh37.gtf with intron-containing pfam file across chrom 1-22, X, Y;
 python fameval.py -i pfam-ccr.bed > pfamshist.txt
-python plotpfam.py -p top100doms -s pfamshist.txt -o $HOME/public_html/randomplots/pfam_hists.pdf
+python plotpfam.py -p top100doms -s pfamshist.txt -c Pfam-A.clans.tsv -o $HOME/public_html/randomplots/pfam_hists.pdf
 #python plotpfam.py -p pfams.txt -s pfamshist.txt -o pfam_hists.pdf
 python gerppfam.py
--- pfam/plotpfam.py
+++ pfam/plotpfam.py
@@ -4,15 +4,23 @@ from matplotlib import pyplot as plt
 from argparse import ArgumentParser
 import numpy as np
 import ast
-#from bokeh.plotting import figure
+import seaborn
+seaborn.set_style(style='white')
 
 parser=ArgumentParser()
-parser.add_argument("-p","--pfam", help="pfam families for histogram")
-parser.add_argument("-s","--stats", help="pfams bp covered in ccr %ile bins")
-parser.add_argument("-o","--output", help="histogram pic")
+parser.add_argument("-p","--pfam", help="pfam families for histogram") # top100doms
+parser.add_argument("-c","--clans", help="pfam clans file, has proper family names in it") # Pfam-A.clans.tsv.gz
+parser.add_argument("-s","--stats", help="pfams bp covered in ccr %ile bins") #pfamshist.txt
+parser.add_argument("-o","--output", help="histogram pic") #pfam_hists.pdf
 args=parser.parse_args()
 with open(args.pfam) as f:
     pfams=[_ for _ in f]
+families={}
+with open(args.clans) as f:
+    for line in f:
+        fields=line.strip().split("\t")
+        families[fields[3]]=fields[4] # pfam id is key, true family name is value
+
 count=len(pfams)
 stats=open(args.stats,"r")
 bins=range(10,101,10)
@@ -36,24 +44,31 @@ for pfam in pfams:
         pfam2=fields[0].strip()
         dbins=ast.literal_eval(fields[1]) #(dictionary of bins)
         if pfam == pfam2:
+            try:
+                title=families[pfam]
+            except KeyError:
+                title=pfam
             tccr=[[i,0] for i in bins]
             totlen=float(fields[2])
             for i in dbins:
                 for j in range(0,len(tccr)):
                     if tccr[j][0]==i:
                         tccr[j][1]=(dbins[i]/totlen)
-            vals=[i[1] for i in tccr]; binar=[i[0] for i in tccr]
-            print vals, binar
-            width = (binar[1]-binar[0])
-            axarr[ct].bar(binar, vals, width = width, color = 'r', alpha = 0.7)
+            vals=[i[1] for i in tccr]; binar=[i[0]-5 for i in tccr] # start at 0 and position bar in the middle of the bin
+            #binar.insert(0,0)
+            print vals, [i + 5 for i in binar]
+            width = (binar[1]-binar[0])+.1
+            axarr[ct].bar(binar, vals, width = width, color = 'b', alpha = 0.7)
+            axarr[ct].xaxis.set_ticks(np.arange(0, 110, 10)) #0-100
+            axarr[ct].axhline(0.1, color = 'k')
             #axarr[ct].plot(binar, vals)
-            axarr[ct].set_title(pfam)
+            axarr[ct].set_title(title)
             axarr[ct].set_xlim(0,100) #10,100
             ct+=1
     stats.seek(0)
 #[make_axes_visible(i) for i in axarr]
 plt.tight_layout()
 plt.subplots_adjust(hspace=0.7)
-[i.set_xlabel('CCR %ile') for i in axarr]
+[i.set_xlabel('CCR Percentile') for i in axarr]
 [i.set_ylabel('Frequency') for i in axarr]
 plt.savefig(args.output, bbox_inches='tight')
