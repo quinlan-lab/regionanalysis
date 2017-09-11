e4bfb1186ffa7101085fa68603c7b89b48c9a3eb
Jim Havrilla <u1021864@kingspeak23.wasatch.peaks>
2017-09-11 03:02:12 -0600

    minor fixes to regression plot and count func vars

--- regression.py
+++ regression.py
@@ -5,6 +5,7 @@ from matplotlib import pyplot as plt
 import cPickle as pickle
 import numpy as np
 import seaborn as sns
+sns.set_style('white')
 import statsmodels.api as sm
 from statsmodels.stats.outliers_influence import OLSInfluence
 import pandas as pd
@@ -22,7 +23,7 @@ topcpg,topcov,topresid,toppct=[],[],[],[]
 for gene in iterable:
     cpg=float(gene['cpg']);
     cov=float(gene['cov_score']);
-    resid=float(gene['cov_cpg_resid']);
+    resid=float(gene['resid']);
     pct=float(gene['weighted_pct'])
     totcpg.append(cpg); totcov.append(cov); totresid.append(resid); totpct.append(pct)
     if pct > 99:
@@ -48,14 +49,14 @@ ax0.set_ylim(0,max(totcov))
 colors = [(1, 1, 1), (0.627451, 0.12549, 0.941176), (0.254902, 0.411765, 0.882353), (0.603922, 0.803922, 0.196078), (1, 0.647059, 0,), (1, 0, 0)]
 cmap_name='mymap'
 cm = LinearSegmentedColormap.from_list(cmap_name, colors)
-g=ax0.hexbin(totcpg, totcov, cmap=cm, bins='log', alpha=0.5)
+g=ax0.hexbin(totcpg, totcov, cmap=cm, bins='log', alpha=0.5, mincnt=1)
 ax0.set_xlabel('CpG proportion');ax0.set_ylabel('Sum of coverage fractions')
 def y_format(x,y):
     return '{:.0f}'.format(10**x-1) # not 100% accurate binning, but the -1 is so we can label the bottom of the colorbar as 0, doesn't throw off calc by much
     
 counts,edges=np.histogram(g.get_array(),bins=8)
-cbar = fig.colorbar(g, ax=ax0, orientation='vertical', extend='both', extendrect=True, drawedges=True, ticks=edges, format=FuncFormatter(y_format))
-cbar.set_label('Number of Regions', rotation=270)
+cbar = fig.colorbar(g, ax=ax0, orientation='vertical', extend='both', extendrect=True, drawedges=False, ticks=edges, format=FuncFormatter(y_format))
+cbar.set_label('Number of Regions', rotation=270, labelpad=20)
 #ax0.plot(topcpg,topcov,'b.')
 x=[0,np.max(totcpg)]
 y=[intercept,cpgcoef*np.max(totcpg)]
@@ -68,5 +69,5 @@ ax0.set_ylabel('Sum of fraction of exomes in ExAC\n with 10x coverage for each b
 #ax1.set_ylabel('Studentized Residuals')
 
 plt.tight_layout()
-plt.savefig("plots/regression.pdf", bbox_inches='tight')
+plt.savefig('/uufs/chpc.utah.edu/common/home/u1021864/public_html/randomplots/regression.pdf', bbox_inches='tight')
 plt.close()
