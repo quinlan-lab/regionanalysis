8439b00c5296f34871bf90dfd69ff79963ce94ea
Jim Havrilla <u1021864@kingspeak23.wasatch.peaks>
2017-09-24 16:59:53 -0600

    ignoring genes where max CCR is 0, aka only variants exist, since that is impossible

--- genevccr.py
+++ genevccr.py
@@ -42,9 +42,14 @@ with gzip.open('essentials/gnomadbased-ccrs.bed.gz', 'rb') as ccrfile:
             if genes[line[3]][1] < ccr:
                 genes[line[3]][1] = ccr
         except KeyError:
-            exit(1)
+            pass
 
-lengths,ccrs=zip(*genes.values())
+lengths,ccrs=[],[]
+for i in genes:
+    if genes[i][1] != 0:
+        lengths.append(genes[i][0])
+        ccrs.append(genes[i][1])
+
 fig, ax = plt.subplots()
 r, pval = pearsonr(ccrs, lengths)
 ax.scatter(ccrs, lengths, alpha = 0.3, s = 1, label = "r = " + str(r) + "\np-value = " + str(pval))
@@ -52,6 +57,7 @@ m, b = np.polyfit(ccrs, lengths, 1)
 ccrs=np.array(ccrs)
 ax.plot(ccrs, m*ccrs + b, 'k-')
 plt.legend(loc='best')
+#plt.ylim(0,20000)
 #g=ax.hexbin(ccrs, lengths, cmap=cm, alpha=0.5, mincnt=1) #, bins='log')
 #colors = [(0.627451, 0.12549, 0.941176), (0.254902, 0.411765, 0.882353), (0.603922, 0.803922, 0.196078), (1, 0.647059, 0,), (0, 0, 0)]
 #cmap_name='mymap'
