0ad29c92b6377bb253d0c2b23e067be3d086af73
Jim Havrilla <u1021864@kingspeak23.wasatch.peaks>
2017-10-13 16:49:00 -0600

    changed to genome based pfam definitions, because there are multi exon Pfam definitions

--- pfam/pfamvccr.sh
+++ pfam/pfamvccr.sh
@@ -23,16 +23,15 @@ if [ ! -s Pfam-A.clans.tsv ]; then
     wget ftp://ftp.ebi.ac.uk/pub/databases/Pfam/current_release/Pfam-A.clans.tsv.gz
     gunzip Pfam-A.clans.tsv.gz
 fi
-bedtools intersect -a ../exacresiduals/gnomad10x.5-ccrs.bed.gz -b pfam.exonic.bed -wao > ccr-pfam.bed # pfam.bed is GRCh37.gtf with intron-containing pfam file across chrom 1-22, X, Y;
+bedtools intersect -a ../exacresiduals/gnomad10x.5-ccrs.bed.gz -b pfam.genome.gene.bed -wao > ccr-pfam.bed # pfam.bed is GRCh37.gtf with intron-containing pfam file across chrom 1-22, X, Y;
 cat ccr-pfam.bed | python ccrpfam.py
 paste tmp/nodomccrs tmp/domccrs | python ../hist.py -o pfam_dist.pdf -t "gnomad based CCR v Pfam" -l "nodoms" "doms" # gnomad based CCR
 awk '$14>90' ccr-pfam.bed | cut -f 18 | sort | uniq -c | sort -k1,1nr | head -100 | sed '1d' | sed 's/^\s*\S*//g' > top100doms #top 100 domains
-bedtools intersect -b ../exacresiduals/gnomad10x.5-ccrs.bed.gz -a pfam.exonic.bed -u | cut -f 4 | sort | uniq > pfams.txt
-#5117/5416 uniq pfams covered by gnomad-ccrs.bed.gz
+bedtools intersect -b ../exacresiduals/gnomad10x.5-ccrs.bed.gz -a pfam.genome.gene.bed -u | cut -f 4 | sort | uniq > pfams.txt
 #
 # histograms of ccr dists across pfams #
 #
-bedtools intersect -a pfam.exonic.bed -b ../exacresiduals/gnomad10x.5-ccrs.bed.gz -wb | sort -k4,4 > pfam-ccr.bed # pfam.bed is GRCh37.gtf with intron-containing pfam file across chrom 1-22, X, Y;
+bedtools intersect -a pfam.genome.gene.bed -b ../exacresiduals/gnomad10x.5-ccrs.bed.gz -wb | sort -k4,4 > pfam-ccr.bed # pfam.bed is GRCh37.gtf with intron-containing pfam file across chrom 1-22, X, Y;
 python fameval.py -i pfam-ccr.bed > pfamshist.txt
 python plotpfam.py -p top100doms -s pfamshist.txt -c Pfam-A.clans.tsv -o $HOME/public_html/randomplots/pfam_hists.pdf
 #python plotpfam.py -p pfams.txt -s pfamshist.txt -o pfam_hists.pdf
@@ -50,7 +49,7 @@ zcat $HOME/analysis/essentials/gnomadbased-ccrs.bed.gz | awk '{n=split($7,a,/,/)
 bedtools intersect -a <(awk '$NF >= 95' ccrsingenomespace.bed) -b pfam.genome.gene.bed -v > topnonpfamccrs.bed
 bedtools intersect -a <(awk '$NF <= 20 && $NF != 0' ccrsingenomespace.bed) -b pfam.genome.gene.bed -v > bottomnonpfamccrs.bed
 bedtools intersect -a <(awk '$NF >= 95' ccrsingenomespace.bed) -b pfam.genome.gene.bed -u > toppfamccrs.bed
-python nodomplot.py topnonpfamccrs.bed bottomnonpfamccrs.bed pfam.exonic.bed $HOME/analysis/exacresiduals/flatexome.bed $HOME/public_html/randomplots/nodom.pdf > output
+python nodomplot.py topnonpfamccrs.bed bottomnonpfamccrs.bed pfam.genome.gene.bed $HOME/analysis/exacresiduals/flatexome.bed $HOME/public_html/randomplots/nodom.pdf > output
 
 # generate table of:
 TOT=$(wc -l toppfamccrs.bed)
